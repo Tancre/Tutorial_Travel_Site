8459a4221b608058f4f3dd455e20ba30777a0d72
Tancre <outisisout@gmail.com>
2020-07-23 19:04:59 +0200

    completed sticky header functionality

--- app/assets/js/modules/RevealOnScroll.js
+++ app/assets/js/modules/RevealOnScroll.js
@@ -1,5 +1,5 @@
  import $ from 'jquery';
- import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints'
+ import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
  class RevealOnScroll {
  	constructor(els, offset){
